394591a32fb7e7d11d69b00b8264f35c3daf67d1
aircraftman3 <aircraftman3@gmail.com>
2019-05-23 17:02:29 +0200

    - last commit

--- main.js
+++ main.js
@@ -15,5 +15,14 @@ $(".nav-item, .dropdown").on("mouseleave", function () {
 });
 
 
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
 
 
